d6ab604bd073baa9178e6fc78abb3d204205744c
Manel Brull <mbrull88@gmail.com>
2014-02-01 10:40:53 +0100

    Person set for hibernate

--- src/com/mabrupi/Person.java
+++ src/com/mabrupi/Person.java
@@ -1,5 +1,45 @@
 package com.mabrupi;
 
 public class Person {
+	private int idPerson;
+	private String name;
+	private String surname;
+	private int age;
+	
+	public Person(){}
+
+	public int getIdPerson() {
+		return idPerson;
+	}
+
+	public void setIdPerson(int idPerson) {
+		this.idPerson = idPerson;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	};
+	
+	
 
 }
